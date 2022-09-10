586bd0a63f5b6036ce1c8665f25e5bfbc54758e0
EhtishamFarman1122 <110455209+EhtishamFarman1122@users.noreply.github.com>
2022-09-10 14:46:42 +0500

    changes

--- includes/db.php
+++ includes/db.php
@@ -8,5 +8,5 @@
         define(strtoupper($key), $value);
     }
 
-    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME
 ?>
