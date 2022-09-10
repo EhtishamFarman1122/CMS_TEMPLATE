8afb91d18ea101a796ab47a4691177488fea90bf
EhtishamFarman1122 <110455209+EhtishamFarman1122@users.noreply.github.com>
2022-09-10 17:08:30 +0500

    ccc

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,10 @@
 <?php 
-    
+    $db['db_host'] = 'localhost';
+    $db['db_user'] = 'root';
+    $db['db_pass'] = '';
+    $db['db_name'] = 'cms';
+
+    foreach ($db as $key => $value){
         define(strtoupper($key), $value);
     }
 
